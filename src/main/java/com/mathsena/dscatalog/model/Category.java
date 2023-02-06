03d19825c7e5420b493cae869826aa707ebf5035
MathSena <MathSena07@hotmail.com>
2023-02-05 22:22:58 -0300

    Create Category Service and Category Repository

--- src/main/java/com/mathsena/dscatalog/model/Category.java
+++ src/main/java/com/mathsena/dscatalog/model/Category.java
@@ -2,6 +2,7 @@ package com.mathsena.dscatalog.model;
 
 import lombok.*;
 
+import javax.persistence.*;
 import java.io.Serializable;
 
 @NoArgsConstructor
@@ -9,9 +10,13 @@ import java.io.Serializable;
 @Getter
 @Setter
 @EqualsAndHashCode
+@Entity
+@Table(name="tb_category")
 public class Category implements Serializable {
     private static final long serialVersionUID = 1l;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
