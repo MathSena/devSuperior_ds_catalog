46047a2aa39ae67e2d923164b2a24182c1ddb66c
MathSena <MathSena07@hotmail.com>
2023-02-10 13:26:04 -0300

    Put Category

--- src/main/java/com/mathsena/dscatalog/services/CategoryService.java
+++ src/main/java/com/mathsena/dscatalog/services/CategoryService.java
@@ -3,14 +3,14 @@ package com.mathsena.dscatalog.services;
 import com.mathsena.dscatalog.model.Category;
 import com.mathsena.dscatalog.model.dto.CategoryDTO;
 import com.mathsena.dscatalog.repositories.CategoryRepository;
-import com.mathsena.dscatalog.services.exceptions.EntityNotFoundException;
+import com.mathsena.dscatalog.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
+
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -42,5 +42,21 @@ public class CategoryService {
         entity.setName(dto.getName());
         entity = repository.save(entity);
         return new CategoryDTO(entity);
+    }
+
+    @Transactional
+    public CategoryDTO update(Long id, CategoryDTO dto) {
+
+        try{
+            Category entity = repository.getOne(id);
+            entity.setName(dto.getName());
+            entity = repository.save(entity);
+            return new CategoryDTO(entity);
+
+        } catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException("id not found" + id);
+
+        }
+
     }
 }
